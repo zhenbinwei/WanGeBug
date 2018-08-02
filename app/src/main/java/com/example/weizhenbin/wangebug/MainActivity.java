1ad729fae85e30220f75ecb97580ee374f4f4794
weizhenbin <weizhenbin@lmbang.com>
2018-08-02 15:06:03 +0800

    测试

--- app/src/main/java/com/example/weizhenbin/wangebug/MainActivity.java
+++ app/src/main/java/com/example/weizhenbin/wangebug/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+       //测试
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
